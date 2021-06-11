1a8fb6a0dc628131db22365dec3cb13ac575d4ec
Charlie Traylor <ctraylor@tql.com>
2021-06-11 13:12:15 -0400

    Add project files.

---
+++ PigDice/PigDice.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PigDice
+{
+    class PigDice
+    {
+        Random rnd = new Random();
+
+        public int[] Play(int ScoreToBeat=0) 
+        {
+            var NumberOfGames = 0;
+            var HighScore = 0;           
+            while(HighScore <= ScoreToBeat)
+            {
+                NumberOfGames++;
+                var score = PlayOneGame();
+                if (score > HighScore)
+                {
+                    HighScore = score;
+                }
+            }
+            return new int[] { HighScore, NumberOfGames };
+            
+            
+        }
+
+        int PlayOneGame()
+        {
+            var score = 0;
+            var die = Roll();
+            while(die !=1)
+            {
+                score += die;
+                die = Roll();
+            }
+            return score;
+        }
+
+        int Roll()
+        {
+            return rnd.Next(1, 7);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public PigDice() { }
+    }
+}
---
+++ PigDice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PigDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pigdice = new PigDice();
+            var result = pigdice.Play(500);
+            Console.WriteLine($"HighScore is {result[0]} for {result[1]} games played");
+        }
+    }
+}
